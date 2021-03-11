13d5d946c55f4a9ad9daa95759604a60a4ff48ec
mstuw <61814551+mstuw@users.noreply.github.com>
2021-03-11 15:22:47 +1100

    add end game activity

---
+++ app/src/main/java/au/edu/federation/itech3107/fedunimillionaire30339249/GameEndActivity.java
@@ -0,0 +1,60 @@
+package au.edu.federation.itech3107.fedunimillionaire30339249;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class GameEndActivity extends AppCompatActivity {
+
+    public static final String EXTRA_WINNINGS_AMOUNT = "au.edu.federation.itech3107.fedunimillionaire30339249.EXTRA_WINNINGS_AMOUNT";
+    public static final String EXTRA_TOTAL_CORRECT = "au.edu.federation.itech3107.fedunimillionaire30339249.EXTRA_TOTAL_CORRECT";
+    public static final String EXTRA_TOTAL_QUESTIONS = "au.edu.federation.itech3107.fedunimillionaire30339249.EXTRA_TOTAL_QUESTIONS";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_end);
+
+        // Get extras.
+        Intent intent = getIntent();
+        double winningsAmount = intent.getDoubleExtra(EXTRA_WINNINGS_AMOUNT, 0);
+        int totalCorrect = intent.getIntExtra(EXTRA_TOTAL_CORRECT, 0);
+        int totalQuestions = intent.getIntExtra(EXTRA_TOTAL_QUESTIONS, 0);
+
+        // Find views.
+        TextView txtStats = findViewById(R.id.txtStats);
+        TextView txtBanner = findViewById(R.id.txtBanner);
+        TextView txtWinnings = findViewById(R.id.txtWinnings);
+        ImageView imageView = findViewById(R.id.imageView);
+
+        // Update views.
+        txtStats.setText(getString(R.string.game_end_stats_format, totalCorrect, totalQuestions));
+        txtBanner.setText(totalCorrect == 0 ? R.string.game_end_lose : R.string.game_end_win);
+
+        int winningsFormatId = totalCorrect == 0 ? R.string.game_end_lose_format : R.string.game_end_format;
+        txtWinnings.setText(getString(winningsFormatId, winningsAmount));
+
+        // Won the jackpot. Answered all questions correctly.
+        if (totalCorrect == totalQuestions) {
+            imageView.setImageResource(R.drawable.ic_trophy); // Use gold trophy image.
+            imageView.setContentDescription(getString(R.string.img_desc_trophy));
+
+        } else {
+            imageView.setImageResource(R.drawable.ic_bronze_medal); // Use bronze medal image.
+            imageView.setContentDescription(getString(R.string.img_desc_bronze_medal));
+
+        }
+
+    }
+
+    public void btnReturnClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+}
--- app/src/main/java/au/edu/federation/itech3107/fedunimillionaire30339249/QuestionActivity.java
+++ app/src/main/java/au/edu/federation/itech3107/fedunimillionaire30339249/QuestionActivity.java
@@ -2,6 +2,8 @@ package au.edu.federation.itech3107.fedunimillionaire30339249;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
@@ -18,12 +20,13 @@ public class QuestionActivity extends AppCompatActivity implements RadioGroup.On
     public static final String EXTRA_QUESTIONS = "au.edu.federation.itech3107.fedunimillionaire30339249.EXTRA_QUESTIONS";
     public static final String EXTRA_CURRENT_QUESTION = "au.edu.federation.itech3107.fedunimillionaire30339249.EXTRA_CURRENT_QUESTION";
     public static final String EXTRA_SAFE_MONEY = "au.edu.federation.itech3107.fedunimillionaire30339249.EXTRA_SAFE_MONEY";
+    public static final String EXTRA_QUESTIONS_ANSWERED_CORRECTLY = "au.edu.federation.itech3107.fedunimillionaire30339249.EXTRA_QUESTIONS_ANSWERED_CORRECTLY";
 
     private ArrayList<Question> questions;
-    private Question currentQuestion;
-    private int currentQuestionIndex;
+    private int currentQuestionIndex; // The current question index (zero-based).
+    private int questionsAnsweredCorrectly; // The number of questions answered correctly.
 
-    private double currentSafeMoney;
+    private double currentSafeMoney; // The amount of money the user has already won.
 
     // Views
     private Button btnConfirm;
@@ -46,38 +49,45 @@ public class QuestionActivity extends AppCompatActivity implements RadioGroup.On
         txtSafeMoney = findViewById(R.id.txtSafeMoney);
         txtQuestionProgress = findViewById(R.id.txtQuestionProgress);
         progressBar = findViewById(R.id.progressBar);
+
         rgChoices = findViewById(R.id.rgChoices);
         rgChoices.setOnCheckedChangeListener(this);
 
-
+        // Get extras.
         Intent intent = getIntent();
         questions = intent.getParcelableArrayListExtra(EXTRA_QUESTIONS);
         currentQuestionIndex = intent.getIntExtra(EXTRA_CURRENT_QUESTION, 0);
         currentSafeMoney = intent.getDoubleExtra(EXTRA_SAFE_MONEY, 0);
-
-        if (currentQuestionIndex >= 0 && currentQuestionIndex < questions.size())
-            currentQuestion = questions.get(currentQuestionIndex);
+        questionsAnsweredCorrectly = intent.getIntExtra(EXTRA_QUESTIONS_ANSWERED_CORRECTLY, 0);
 
         updateView();
     }
 
-    /** Update views to reflect member variables. */
+    /**
+     * Update views to reflect member variables.
+     */
     private void updateView() {
-        txtQuestion.setText(getString(R.string.question_format, currentQuestion.getQuestion()));
-        txtPlayingFor.setText(getString(R.string.question_playing_for_format, currentQuestion.getValue()));
+        Question question = getCurrentQuestion();
+
+        txtQuestion.setText(getString(R.string.question_format, question.getQuestion()));
+        txtPlayingFor.setText(getString(R.string.question_playing_for_format, question.getValue()));
         txtSafeMoney.setText(getString(R.string.safe_money_format, currentSafeMoney));
 
         // Update progress information.
-        progressBar.setProgress(currentQuestionIndex);
+        progressBar.setProgress(currentQuestionIndex + 1);
         progressBar.setMax(questions.size());
-        txtQuestionProgress.setText(getString(R.string.question_progress_format, currentQuestionIndex, questions.size()));
+        txtQuestionProgress.setText(getString(R.string.question_progress_format, currentQuestionIndex + 1, questions.size()));
 
         // Update question answers.
         rgChoices.removeAllViews();
-        for (int index = 0; index < currentQuestion.getChoices().length; index++) {
+        for (int index = 0; index < question.getChoices().length; index++) {
             RadioButton btn = new RadioButton(getApplicationContext());
+
             btn.setTag(index);
-            btn.setText(getString(R.string.question_choice_format, currentQuestion.getChoices()[index]));
+            btn.setTextSize(TypedValue.COMPLEX_UNIT_SP, 18);
+            btn.setTextColor(getResources().getColor(R.color.black, getTheme()));
+            btn.setText(getString(R.string.question_choice_format, question.getChoices()[index]));
+
             rgChoices.addView(btn);
         }
 
@@ -85,28 +95,52 @@ public class QuestionActivity extends AppCompatActivity implements RadioGroup.On
 
     public void btnConfirmClicked(View view) {
         RadioButton checkedAnswer = findViewById(rgChoices.getCheckedRadioButtonId());
-        if (currentQuestion.getCorrectChoice() == (int) checkedAnswer.getTag()) { // Answer was correct.
-            if (currentQuestion.isSafeMoney())
-                currentSafeMoney = currentQuestion.getValue();
+
+        Question question = getCurrentQuestion();
+
+        if (question.getCorrectChoice() == (int) checkedAnswer.getTag()) { // Answer was correct.
+            questionsAnsweredCorrectly++;
+
+            if (question.isSafeMoney())
+                currentSafeMoney = question.getValue();
+
+            if (currentQuestionIndex == questions.size() - 1) { // Reached the end of the questions. User has won.
+                endGame();
+            } else {
+                // Next question...
+                Intent intent = new Intent(this, QuestionActivity.class);
+                intent.putParcelableArrayListExtra(EXTRA_QUESTIONS, questions);
+                intent.putExtra(EXTRA_CURRENT_QUESTION, currentQuestionIndex + 1);
+                intent.putExtra(EXTRA_SAFE_MONEY, currentSafeMoney);
+                intent.putExtra(EXTRA_QUESTIONS_ANSWERED_CORRECTLY, questionsAnsweredCorrectly);
+
+                startActivity(intent);
+
+            }
 
         } else { // Answer was incorrect.
-            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-            startActivity(intent);
-            return;
-
+            endGame();
         }
 
-        Intent intent = new Intent(getApplicationContext(), QuestionActivity.class);
-        intent.putExtra(EXTRA_CURRENT_QUESTION, currentQuestionIndex + 1);
-        intent.putExtra(EXTRA_SAFE_MONEY, currentSafeMoney);
-        intent.putParcelableArrayListExtra(EXTRA_QUESTIONS, questions);
+    }
+
+    /** @return The current question this activity should be displaying / handling. */
+    private Question getCurrentQuestion() {
+        return questions.get(currentQuestionIndex);
+    }
+
+    /** Starts the {@link GameEndActivity} with the required extras needed. Can be used for both winning and losing states. */
+    private void endGame() {
+        Intent intent = new Intent(this, GameEndActivity.class);
+        intent.putExtra(GameEndActivity.EXTRA_WINNINGS_AMOUNT, currentSafeMoney);
+        intent.putExtra(GameEndActivity.EXTRA_TOTAL_QUESTIONS, questions.size());
+        intent.putExtra(GameEndActivity.EXTRA_TOTAL_CORRECT, questionsAnsweredCorrectly);
         startActivity(intent);
     }
 
     @Override
     public void onCheckedChanged(RadioGroup radioGroup, int i) {
         btnConfirm.setEnabled(true);
-
     }
 
 }

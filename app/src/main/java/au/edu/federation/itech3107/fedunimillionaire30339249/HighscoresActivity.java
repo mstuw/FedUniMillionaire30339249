e7598214e84ea1ba9d71c8a197a025d4190916c7
mstuw <61814551+mstuw@users.noreply.github.com>
2021-04-06 16:53:23 +1000

    add ability to delete highscores

--- app/src/main/java/au/edu/federation/itech3107/fedunimillionaire30339249/HighscoresActivity.java
+++ app/src/main/java/au/edu/federation/itech3107/fedunimillionaire30339249/HighscoresActivity.java
@@ -4,9 +4,11 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -28,19 +30,37 @@ public class HighscoresActivity extends AppCompatActivity {
     private int orderBySelection = 0;
     private int sortBySelection = 0;
 
+    private ListView lvHighscores;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_highscores);
 
         adapter = new HighscoresListAdapter(this, R.layout.highscores_item, highscores);
-
-        ListView lvHighscores = findViewById(R.id.lvHighscores);
+        lvHighscores = findViewById(R.id.lvHighscores);
         lvHighscores.setAdapter(adapter);
+
+        // Register list view for long click context menu.
+        registerForContextMenu(lvHighscores);
 
         refreshHighscores();
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        getMenuInflater().inflate(R.menu.highscores_item_actions_menu, menu); // Long click context menu for list view.
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.highscores_menu, menu);
+        return true;
+    }
+
+    /**
+     * Refresh all items in list view with items in database. Sorted by column in ascending or descending order.
+     */
     private void refreshHighscores() {
         try (HighscoresDataSource ds = new HighscoresDataSource(this)) {
 
@@ -67,6 +87,37 @@ public class HighscoresActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Deletes the specified highscore from the database and list view (the specified index is the adapter index).
+     */
+    private void deleteHighscore(int index) {
+        Log.d(TAG, "Deleting highscore item #" + index);
+
+        try (HighscoresDataSource ds = new HighscoresDataSource(this)) {
+            Highscore highscore = adapter.getItem(index);
+
+            if (ds.delete(highscore)) {
+                // Remove item from list view, if item was successfully removed from database.
+                adapter.remove(highscore);
+                adapter.notifyDataSetInvalidated();
+            }
+
+        } catch (Exception e) {
+            Log.e(TAG, "Failed deleting highscore!", e);
+        }
+
+
+    }
+
+    // Called when the "three dot" button is clicked for any item in the list view.
+    public void showItemPopup(View view) {
+        lvHighscores.showContextMenuForChild(view);
+    }
+
+    public void btnReturnClicked(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -106,14 +157,17 @@ public class HighscoresActivity extends AppCompatActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.highscores_menu, menu);
-        return true;
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                deleteHighscore(info.position);
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 
-    public void btnReturnClicked(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
 
 }
